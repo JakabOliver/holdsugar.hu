1966d1801159d5fb70f573737a0a5d9f68268245
Oliver <oliver.jakab.r@gmail.com>
2019-03-30 18:33:31 +0100

    Képek beolvasva

--- index.php
+++ index.php
@@ -3,15 +3,31 @@
 $head=file_get_contents('head.part.html');
 $foot=file_get_contents('foot.part.html');
 
+if(isset($_GET['kepek'])){
+    $files=scandir(__DIR__.'/kepek/');
+    $site='<h2>Képek</h2><section class="row">';
+    foreach($files as $file){
+        if(!in_array($file, array('.', '..'))){
+            $site.='
+            <div class="col-12 col-md-6 col-lg-3 my-2">
+                <a href="'.$file.'" target="_blank">
+                <img class="img-fluid" src="'.$file.'">
+                </a>
+            </div>
+            ';
+        }
+    }
 
-$path='';
-if(!empty($_GET['time']))
-    $path.=$_GET['time'].'/';
-if(!empty($_GET['sub']))
-    $path.=$_GET['sub'].'/';
+}else{
+    $path='';
+    if(!empty($_GET['time']))
+        $path.=$_GET['time'].'/';
+    if(!empty($_GET['sub']))
+        $path.=$_GET['sub'].'/';
+        $site=file_get_contents($path.'index.part.html');
+        
+}
 
-
-$site=file_get_contents($path.'index.part.html');
 
 
 echo $head.$site.$foot;
